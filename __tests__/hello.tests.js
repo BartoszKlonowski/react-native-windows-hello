07922b20715fe03bb3f613773d40cd907eb3f929
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-07-19 17:37:01 +0200

    CI - workflow configuration (#2)
    * Use Clang formatting for native C++ code
    
    The Clang-Format:
    https://clang.llvm.org/docs/ClangFormatStyleOptions.html
    is used in this project to ensure the same common standard is kept for
    the C++ development.
    
    The standard is used mostly by Microsoft but a bit modified.
    It is a mix of Microsoft and GNU mostly with custom settings.
    
    The .clang-format configuration file is placed in the root of Windows
    solution in windows/ directory to ensure that it will be used for
    Windows native side only and won't reach the JS side.
    
    * Configure CI workflow in CircleCI
    
    The full CI workflow will include:
    * checking the formatting and code standard for both JavaScript and C++
    * checking for correct build of the project for both Release and Debug
      configuration for x64 platform
    * checking the correct build of Example project
    
    The workflow diagram can be presented as:
    
    install -> verify-cpp        -> build-project-release-x64 -> build-example
               verify-javascript    build-project-debug-x64
    
    So the Examples are built only for full correct project workflow.
    
    * Downgrade Jest version to 25.1.0
    
    Due to the fact of outdated windows orb in CircleCI it was necessary to
    downgrade the version of Jest used in this project to the one which did
    not require the selected specific versions of Node.
    
    * Verify the JavaScript files from the root
    
    * Configure Git for clang-format
    
    * Simplify the verify-cpp job when configuring clang-format
    
    * Use lower SDK for the windows project
    
    * Add JavaScript Jest tests to the workflow
    
    The tests are currently implemented only as a dummy `hello.tests.js` so
    that it is only checked if tests are running OK and workflow continues.
    
    * Add unit tests for C++ implementation
    
    The C++ unit tests are configured using Google Tests framework:
    * it can be ran in the CLI by launching the executable
      (no need for special scripts especially when using CircleCI)
    * it is well known and fully supported
    
    NOTE: tests added currently to the project are only the dummy
    `tests.cpp` file so that it is checked if the configuration is done well
    and is working.
    
    * Add native UT to workflow and check their formatting
    
    * Add Example application build to the configuration
    
    The Example application is not yet added to the workflow, as there's no
    Example app yet implemented, even no empty scaffolded solution, so the
    workflow would obviously fail.
    However, adding it to the workflow is a matter of few lines as the
    config is ready.

---
+++ __tests__/hello.tests.js
@@ -0,0 +1,7 @@
+/**
+ * A simple test before the lib implementation
+ * It's purpose is to present the test passing and workflow running correctly
+ */
+test("adds 1 + 2 to equal 3", () => {
+  expect(1 + 2).toBe(3);
+});
