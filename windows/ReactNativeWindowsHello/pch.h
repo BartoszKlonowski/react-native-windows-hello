d1db5bab31cf8a26aae63eab39ba0dc12c9bb64c
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-10-07 11:29:35 +0200

    Upgrade the react-native & react-native-windows version to 0.65 (#11)
    * Update react-native and react-native-windows to latest
    
    * Upgrade windows solution to the latest of 0.65.0
    
    * Fix missing unit tests for windows cpp solution
    
    * Fix ESLint error by installing eslint-plugin-prettier
    
    * Fix key mismatch by upgrading the vdeps
    
    * Checkout Cpp unit tests with autocrlf set to false

--- windows/ReactNativeWindowsHello/pch.h
+++ windows/ReactNativeWindowsHello/pch.h
@@ -6,15 +6,7 @@
 #include <restrictederrorinfo.h>
 #include <unknwn.h>
 #include <windows.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
-#include <winrt/Windows.UI.Xaml.Controls.h>
-#include <winrt/Windows.UI.Xaml.Data.h>
-#include <winrt/Windows.UI.Xaml.Interop.h>
-#include <winrt/Windows.UI.Xaml.Markup.h>
-#include <winrt/Windows.UI.Xaml.Navigation.h>
-#include <winrt/Windows.UI.Xaml.h>
+#include <CppWinRTIncludes.h>
 #include <winrt/Windows.Security.Credentials.UI.h>
 
 #include <winrt/Microsoft.ReactNative.h>
